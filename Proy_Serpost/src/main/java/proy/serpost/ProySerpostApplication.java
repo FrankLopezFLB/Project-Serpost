49d8f551147512ffff4d4e854e26cf613505251a
Green <franklopezflb@gmail.com>
2021-10-11 11:11:43 -0500

    proyecto 0.0.1

---
+++ Proy_Serpost/src/main/java/proy/serpost/ProySerpostApplication.java
@@ -0,0 +1,13 @@
+package proy.serpost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProySerpostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProySerpostApplication.class, args);
+	}
+
+}
---
+++ Proy_Serpost/src/test/java/proy/serpost/ProySerpostApplicationTests.java
@@ -0,0 +1,13 @@
+package proy.serpost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProySerpostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
