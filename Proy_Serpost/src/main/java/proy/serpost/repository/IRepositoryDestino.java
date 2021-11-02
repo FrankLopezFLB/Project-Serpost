1cd93737bf538f5898e3409c6133d1e4302062de
Ayrtonjf <ayrtonjf46@gmail.com>
2021-11-02 09:28:11 -0500

    DAWI_2do_Avance_Barriga

---
+++ Proy_Serpost/src/main/java/proy/serpost/repository/IRepositoryDestino.java
@@ -0,0 +1,13 @@
+package proy.serpost.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import proy.serpost.model.Destino;
+
+public interface IRepositoryDestino extends JpaRepository<Destino, String> {
+	Destino findByCodigo(String codigo);
+	
+	@Query(value = "SELECT max(codigo) FROM Destino")
+	String getMaxCodigoDestino();
+}
