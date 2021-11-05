96f8d2bab18922040aafde58b413e3903504f9fd
Ayrtonjf <ayrtonjf46@gmail.com>
2021-11-05 10:18:19 -0500

    DAWI_Avance_2_Ultimo_pinto

--- Proy_Serpost/src/main/java/proy/serpost/repository/IRepositoryReporte.java
+++ Proy_Serpost/src/main/java/proy/serpost/repository/IRepositoryReporte.java
@@ -1,5 +1,18 @@
 package proy.serpost.repository;
 
-public interface IRepositoryReporte {
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import proy.serpost.model.Reporte;
+
+public interface IRepositoryReporte extends JpaRepository<Reporte, String> {
+	
+	@Query(value = "call REPORTEXFECHA(?1,?2)", nativeQuery = true)
+	List<Reporte> spReportexFecha(String fec1, String fec2);
+	
+	@Query(value = "call REPORTEXTIPO(?1)", nativeQuery = true)
+	List<Reporte> spReportexTipo(int tipo);
 
 }
